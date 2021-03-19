0af3f8913071b6a8958359aa73ed41c76c165baa
mdluthfar <80320204+mdluthfar@users.noreply.github.com>
2021-03-19 16:14:34 +0600

    new change

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+ 
+document.querySelector("#expandNav").addEventListener("click",function(){
+	document.querySelector(".menu").classList.toggle("showkoro");
+	
+});
+
+document.querySelector("#subssubmit").addEventListener("click",function(){
+
+		alert("Your subssubmited is completed");
+	
+});
+
+
+
+ 
+
+
